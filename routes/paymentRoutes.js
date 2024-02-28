927ea17a41788ae88e815a7ba8beb6387c0ff522
Shrikrushna Patil <shri.medisage@gmail.com>
2024-02-28 22:59:03 +0530

    add rezorpay Payment Api

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,5 +1,5 @@
 express =require("express");
-const { makePayment,find,getAll,edit } = require("../controllers/paymentController");
+const { makePayment,find,getAll,edit,order,validatePayment } = require("../controllers/paymentController");
 const {protect,adminProtect} =require("../middlewares/authMiddleware");
 
 
@@ -9,4 +9,9 @@ router.route("/make-payment").post(protect,makePayment );
 router.route("/edit-payment/:id").post(adminProtect,edit);
 router.route("/get-payment-details/:id").get(protect,find);
 router.route("/get-all-payments").get(adminProtect,getAll);
+
+//
+router.route("/rezopay-order").post(order);
+router.route("/rezopay-order/validate").post(validatePayment);
+
 module.exports=router;
